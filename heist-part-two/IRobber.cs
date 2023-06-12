4eb2fa2c4cfbc596f895fb00d41149b28e743b8f
David Profitt <dlprofitt@gmail.com>
2023-06-11 22:26:50 -0400

    step 2 complete

---
+++ heist-part-two/IRobber.cs
@@ -0,0 +1,7 @@
+public interface IRobber
+{
+    public string Name { get; set; }
+    public int SkillLevel { get; set; }
+    public int PercentageCut { get; set; }
+    public void PerformSkill(Bank bank);
+}
