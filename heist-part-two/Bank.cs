8caf5de59331aa9a896511b6141cb5cda140833e
David Profitt <dlprofitt@gmail.com>
2023-06-11 22:17:18 -0400

    step 1 complete, set up Bank class

--- heist-part-two/Bank.cs
+++ heist-part-two/Bank.cs
@@ -4,5 +4,18 @@ public class Bank
     public int AlarmScore { get; set; }
     public int VaultScore { get; set; }
     public int SecurityGuardScore { get; set; }
-    public bool IsSecure { get; set; }
+    public bool IsSecure
+    {
+        get
+        {
+            if (AlarmScore > 0 || VaultScore > 0 || SecurityGuardScore > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
 }
